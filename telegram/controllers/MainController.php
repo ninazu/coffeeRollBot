074601e678a44ed81e4236ce747b4072a9705a73
ninazu <ninazu@ukr.net>
2019-02-20 16:03:11 +0000

    #

--- telegram/controllers/MainController.php
+++ telegram/controllers/MainController.php
@@ -4,8 +4,6 @@ namespace telegram\controllers;
 
 use Telegram;
 use vendor\ninazu\framework\Component\BaseController;
-use vendor\ninazu\framework\Component\Telegram\v2\Message\CallbackQuery;
-use vendor\ninazu\framework\Component\Telegram\v2\Message\DummyMessage;
 use vendor\ninazu\framework\Component\Telegram\v2\Message\Message;
 
 class MainController extends BaseController {
@@ -14,17 +12,21 @@ class MainController extends BaseController {
 
 	public function actionMain() {
 		$this->bot = Telegram::$app->bot;
-
 		$message = $this->bot->request->getMessage();
 		$response = null;
 
 		switch (true) {
 			case $message instanceof Message:
-				$response = $this->bot->response->sendMessage($message->user->id, $this->bot->request->getRawData());
+
+				$response = $this->bot->response->sendMessage($message->chat->id, $this->bot->request->getRawData());
 
 				break;
 		}
 
 		return $response;
+	}
+
+	private function getCommand() {
+
 	}
 }
